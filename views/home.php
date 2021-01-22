f0ef9389e64603ecad206eb02f41553181e12f4a
Johan Borg <johanborg81@hotmail.com>
2021-01-22 11:47:56 +0900

    Add media queries for tablet and desktop

--- views/home.php
+++ views/home.php
@@ -21,41 +21,47 @@ $teams = new TeamsController();
 
 
 ?>
-<section class="section__left">
-    <section class="random__player">
-        <h2><?= $player['fullName']; ?></h2>
-        <ul class="player__info">
-            <li><span>Birthdate:</span> <?= $player['birthDate']; ?></li>
-            <li><span>Age:</span> <?= $player['currentAge']; ?></li>
-            <li><span>City:</span> <?= $player['birthCity']; ?></li>
-            <li><span>Height:</span> <?= $player['height']; ?></li>
-            <li><span>Number:</span> <?= $player['primaryNumber']; ?></li>
-            <li><span>Position:</span> <?= $player['primaryPosition']['name']; ?></li>
-            <li><span>Shoots/Catches:</span> <?= $player['shootsCatches']; ?></li>
-            <li><span>Team:</span> <?= $player['currentTeam']['name']; ?></li>
-        </ul>
-    </section>
-    <?php
+<div class="container">
+    <div class="grids">
+        <section class="section__left">
+            <section class="random__player">
+                <h2><?= $player['fullName']; ?></h2>
+                <ul class="player__info">
+                    <li><span>Birthdate:</span> <?= $player['birthDate']; ?></li>
+                    <li><span>Age:</span> <?= $player['currentAge']; ?></li>
+                    <li><span>City:</span> <?= $player['birthCity']; ?></li>
+                    <li><span>Height:</span> <?= $player['height']; ?></li>
+                    <li><span>Number:</span> <?= $player['primaryNumber']; ?></li>
+                    <li><span>Position:</span> <?= $player['primaryPosition']['name']; ?></li>
+                    <li><span>Shoots/Catches:</span> <?= $player['shootsCatches']; ?></li>
+                    <li><span>Team:</span> <?= $player['currentTeam']['name']; ?></li>
+                </ul>
+            </section>
+            <?php
 
-    include_once '../inc/home/current-stats.php';
-    include_once '../inc/home/last-stats.php';
-    include_once '../inc/home/career-stats.php';
-    include_once '../inc/home/playoff-stats.php';
+            include_once '../inc/home/current-stats.php';
+            include_once '../inc/home/last-stats.php';
+            include_once '../inc/home/career-stats.php';
+            include_once '../inc/home/playoff-stats.php';
 
-    ?>
-    <section class="buttons">
-            <button id="current__season">Current Season</button>
-            <button id="last__season">Last Season</button>
-            <button id="career__stats">Career Stats</button>
-            <button id="playoff__stats">Playoff Stats</button>
-    </section>
-</section>
-<section class="section__right">
-    <section class="teams">
-        <ul class="teams__list">
-            <?php $teams->team_list(); ?>
-        </ul>
-    </section>
-    <?php
+            ?>
+            <section class="buttons">
+                <button id="current__season">Current Season</button>
+                <button id="last__season">Last Season</button>
+                <button id="career__stats">Career Stats</button>
+                <button id="playoff__stats">Playoff Stats</button>
+            </section>
+        </section>
+        <section class="section__right">
+            <section class="teams">
+                <ul class="teams__list">
+                    <?php $teams->team_list(); ?>
+                </ul>
+            </section>
+        </section>
+    </div>
+</div>
 
-    require_once '../inc/footer.php';
+<?php
+
+require_once '../inc/footer.php';
