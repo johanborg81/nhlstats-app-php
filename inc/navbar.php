d3858a8eb5c01916a7078cea5d844b01bf64e816
Johan Borg <johanborg81@hotmail.com>
2021-01-22 15:52:16 +0900

    Remove obsolete files and edit the navbar

--- inc/navbar.php
+++ inc/navbar.php
@@ -2,7 +2,6 @@
     <div class="navbar__nav">
         <i class="fas fa-hockey-puck"></i>
         <a href="home.php">Home</a>
-        <a href="about.php">About</a>
         <i class="fas fa-hockey-puck"></i>
     </div>
 </navbar>
